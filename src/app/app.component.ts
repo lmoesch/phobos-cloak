fd808194df8c23359ec59a00bb9b63cf32a83f2f
Lucas Mösch <lucasmoesch@live.de>
2024-05-05 23:05:42 +0200

    Added Ph-Elements submodule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PhElementsModule } from '../../lib/phobos-elements/ph-elements.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    PhElementsModule
+  ],
   template: `
     <h1>Welcome to {{title}}!</h1>
+
+    <ph-window></ph-window>
 
     <router-outlet />
   `,
   styles: [],
 })
+
 export class AppComponent {
   title = 'phobos-cloak';
 }
