d243a8142cb74c59485768f800f2f8b236f760ed
Lucas Mösch <lmoesch@ukaachen.de>
2025-08-28 12:53:56 +0200

    Preliminary integration of cluster control

--- apps/frontend/src/app/cluster-control/cluster-control.component.ts
+++ apps/frontend/src/app/cluster-control/cluster-control.component.ts
@@ -1,10 +1,14 @@
 import { Component } from "@angular/core";
 import { MatrixComponent } from "../common/matrix/matrix.component";
+import { PhWindow } from "@phobos/elements";
+import { ClusterMatrixComponent } from "../infrastructure/ui/cluster-matrix/cluster-matrix.component";
 
 @Component({
   selector: "app-cluster-control",
   imports: [
-    MatrixComponent
+    ClusterMatrixComponent,
+    MatrixComponent,
+    PhWindow
   ],
   templateUrl: "./cluster-control.component.html",
   styleUrls: ["./cluster-control.component.scss"]
--- apps/frontend/src/app/infrastructure/ui/cluster-matrix/cluster-matrix.component.ts
+++ apps/frontend/src/app/infrastructure/ui/cluster-matrix/cluster-matrix.component.ts
@@ -1,8 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { PhSliderVertical } from '@phobos/elements';
 
-import { PhobosBackendService } from '../../../backend/phobos.backend.service';
-
 @Component({
   selector: 'ph-cloak-cluster-matrix',
   imports: [
@@ -16,35 +14,35 @@ export class ClusterMatrixComponent {
   @Input() matrix: number[] = [0.166, 0.166, 0.166, 0.166, 0.166, 0.166];
   @Output() matrixChange: EventEmitter<number[]> = new EventEmitter<number[]>();
 
-  constructor(private readonly phobos: PhobosBackendService) { }
+  constructor() { }
   
   ngAfterViewInit() {
-    this.phobos.onOpen.subscribe(async () => {
-      const matrix = (await this.phobos.request({getClusterMixingMatrix: {}})).getClusterMixingMatrix?.matrix;
-      this.matrix = matrix!;
-    });
+    // this.phobos.onOpen.subscribe(async () => {
+    //   const matrix = (await this.phobos.request({getClusterMixingMatrix: {}})).getClusterMixingMatrix?.matrix;
+    //   this.matrix = matrix!;
+    // });
 
-    this.phobos.onRequest.subscribe(async (request) => {
-      if (request.request.setClusterMixingMatrix) {
-        this.matrix = request.request.setClusterMixingMatrix.matrix;
-      }
-    });
+    // this.phobos.onRequest.subscribe(async (request) => {
+    //   if (request.request.setClusterMixingMatrix) {
+    //     this.matrix = request.request.setClusterMixingMatrix.matrix;
+    //   }
+    // });
   }
 
   onSliderValueChange(index: number, value: number) {
-    this.matrix[index] = value / 100;
-    const sum = this.matrix.reduce((a, b, currentIndex) => a + b, 0);
-    const diff = sum - (value / 100);
-    const remainder = 1 - (value / 100);
+    // this.matrix[index] = value / 100;
+    // const sum = this.matrix.reduce((a, b, currentIndex) => a + b, 0);
+    // const diff = sum - (value / 100);
+    // const remainder = 1 - (value / 100);
 
-    let weight = remainder / diff;
-    if (isNaN(weight)) {
-      weight = 0;
-    } else if (! isFinite(weight)) {
-      weight = 1;
-    }
+    // let weight = remainder / diff;
+    // if (isNaN(weight)) {
+    //   weight = 0;
+    // } else if (! isFinite(weight)) {
+    //   weight = 1;
+    // }
 
-    this.matrix = this.matrix.map((currentValue, currentIndex) => currentIndex === index ? value / 100 : currentValue * weight);
-    this.matrixChange.emit(this.matrix);
+    // this.matrix = this.matrix.map((currentValue, currentIndex) => currentIndex === index ? value / 100 : currentValue * weight);
+    // this.matrixChange.emit(this.matrix);
   }
 }
