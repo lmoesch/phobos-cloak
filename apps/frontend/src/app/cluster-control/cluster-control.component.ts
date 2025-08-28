3eb13f8112a9ab4b1095eb783743d991de52b2ce
Lucas Mösch <lmoesch@ukaachen.de>
2025-08-28 16:58:32 +0200

    Added matrixChange eventlistener

--- apps/frontend/src/app/cluster-control/cluster-control.component.ts
+++ apps/frontend/src/app/cluster-control/cluster-control.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { MatrixComponent } from "../common/matrix/matrix.component";
 import { PhWindow } from "@phobos/elements";
 import { ClusterMatrixComponent } from "../infrastructure/ui/cluster-matrix/cluster-matrix.component";
+import { MatrixService } from "../common/matrix/matrix.service";
 
 @Component({
   selector: "app-cluster-control",
@@ -14,5 +15,7 @@ import { ClusterMatrixComponent } from "../infrastructure/ui/cluster-matrix/clus
   styleUrls: ["./cluster-control.component.scss"]
 })
 export class ClusterControlComponent {
-    
+    constructor(
+      public matrix: MatrixService
+    ) {}
 }
--- apps/frontend/src/app/infrastructure/ui/cluster-matrix/cluster-matrix.component.ts
+++ apps/frontend/src/app/infrastructure/ui/cluster-matrix/cluster-matrix.component.ts
@@ -15,34 +15,21 @@ export class ClusterMatrixComponent {
   @Output() matrixChange: EventEmitter<number[]> = new EventEmitter<number[]>();
 
   constructor() { }
-  
-  ngAfterViewInit() {
-    // this.phobos.onOpen.subscribe(async () => {
-    //   const matrix = (await this.phobos.request({getClusterMixingMatrix: {}})).getClusterMixingMatrix?.matrix;
-    //   this.matrix = matrix!;
-    // });
-
-    // this.phobos.onRequest.subscribe(async (request) => {
-    //   if (request.request.setClusterMixingMatrix) {
-    //     this.matrix = request.request.setClusterMixingMatrix.matrix;
-    //   }
-    // });
-  }
 
   onSliderValueChange(index: number, value: number) {
-    // this.matrix[index] = value / 100;
-    // const sum = this.matrix.reduce((a, b, currentIndex) => a + b, 0);
-    // const diff = sum - (value / 100);
-    // const remainder = 1 - (value / 100);
+    this.matrix[index] = value / 100;
+    const sum = this.matrix.reduce((a, b, currentIndex) => a + b, 0);
+    const diff = sum - (value / 100);
+    const remainder = 1 - (value / 100);
 
-    // let weight = remainder / diff;
-    // if (isNaN(weight)) {
-    //   weight = 0;
-    // } else if (! isFinite(weight)) {
-    //   weight = 1;
-    // }
+    let weight = remainder / diff;
+    if (isNaN(weight)) {
+      weight = 0;
+    } else if (! isFinite(weight)) {
+      weight = 1;
+    }
 
-    // this.matrix = this.matrix.map((currentValue, currentIndex) => currentIndex === index ? value / 100 : currentValue * weight);
-    // this.matrixChange.emit(this.matrix);
+    this.matrix = this.matrix.map((currentValue, currentIndex) => currentIndex === index ? value / 100 : currentValue * weight);
+    this.matrixChange.emit(this.matrix);
   }
 }
